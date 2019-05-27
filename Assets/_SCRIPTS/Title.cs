257d2f92ff31fd6d0a3b50c374b9bb1032c2c1db
Kyle Qian <kylecqian@gmail.com>
2019-05-27 02:56:45 -0700

    DebugRose

--- Assets/_SCRIPTS/Title.cs
+++ Assets/_SCRIPTS/Title.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    int debugRoseTaps;
+
     public void LoadIntro()
     {
         SceneManager.LoadScene("Intro", LoadSceneMode.Single);
@@ -11,5 +13,13 @@ public class Title : MonoBehaviour
     public void LoadResources()
     {
         SceneManager.LoadScene("Resources", LoadSceneMode.Single);
+    }
+
+    public void DebugRose()
+    {
+        if (++debugRoseTaps >= 20)
+        {
+            SceneManager.LoadScene("Root", LoadSceneMode.Single);
+        }
     }
 }
